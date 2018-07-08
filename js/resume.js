a097b17470152612eb45c76aa997be3bdbbb8244
张毅 <zhangyi@zhangyideMBP.lan>
2018-07-08 18:45:31 +0800

    newdiary

--- js/resume.js
+++ js/resume.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$("[data-toggle='popover']").popover();
+$("[data-toggle='popover']").popover({trigger:'hover'});
 $('body').on('click', function (event) {
     var target = $(event.target); //当前点击的元素
     console.log(target.hasClass('pop'))
@@ -11,12 +11,12 @@ $('body').on('click', function (event) {
         $('.pop').popover('hide');      // 当点击body的非弹出框相关的内容的时候，关闭所有popover
     }
 });
-$(".pop").click(function () {
-    $('.pop').popover('hide');          // 当点击一个按钮的时候把其他的所有内容先关闭。
-    console.log(111)
-    console.log($(this).popover())
-    $(this).popover('toggle');          // 然后只把自己打开。
-    $(this).popover(); 
-    console.log($(this))
-});
+//$(".pop").click(function () {
+//    $('.pop').popover('hide');          // 当点击一个按钮的时候把其他的所有内容先关闭。
+//    console.log(111)
+//    console.log($(this).popover())
+//    $(this).popover('toggle');          // 然后只把自己打开。
+//    $(this).popover(); 
+//    console.log($(this))
+//});
 
