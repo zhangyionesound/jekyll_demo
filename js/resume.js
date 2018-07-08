0b931836681bec2c93bf319967ae9419ec713c9d
张毅 <zhangyi@zhangyideMBP.lan>
2018-07-08 16:28:43 +0800

    newdiary

--- js/resume.js
+++ js/resume.js
@@ -4,5 +4,18 @@
  * and open the template in the editor.
  */
 $("[data-toggle='popover']").popover();
-
-
+$('body').on('click', function (event) {
+    var children = $(".popover").find('*');  //查询出popover下所有的子孙元素
+    var target = $(event.target); //当前点击的元素
+    var bool = true;
+    for (var i = 0; i < children.length; i++) {
+        if (children[i] === event.target) {   //判断点击的元素是否在popover中
+            bool = false;
+        } else if (target.hasClass('node_name')) { //popover点击触发的元素，根据targetId的样式
+            bool = false;
+        }
+    }
+    if (bool) {
+        $(targetId).popover('hide');
+    }
+});
